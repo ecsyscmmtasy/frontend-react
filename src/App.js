21f21a56a5f0a7e74268f32ba6d1ac2eebacf019
ecsyscmmtasy <nmabov@gmail.com>
2026-06-01 14:13:27 +0500

    feat: подготовил верстку

--- src/App.js
+++ src/App.js
@@ -1,33 +1,12 @@
-import React from 'react';
-import './App.css';
-import Header from './components/Header/Header';
-import Counter from './components/Counter/Counter';
-import UserList from './components/UserList/UserList';
-
-function App() {
-  return (
-    <div className="app">
-      <Header title="React Практика" />
-
-      <main className="app__main">
-        <section className="app__section">
-          <h2>Задание 1: Счётчик</h2>
-          <p className="app__description">
-            Изучите компонент Counter — как работает useState и обработка событий.
-          </p>
-          <Counter />
-        </section>
-
-        <section className="app__section">
-          <h2>Задание 2: Список пользователей</h2>
-          <p className="app__description">
-            Изучите компонент UserList — как работает useEffect и загрузка данных.
-          </p>
-          <UserList />
-        </section>
-      </main>
-    </div>
-  );
-}
-
-export default App;
+const mockArcticles = [
+  {
+    title: "Первая статья",
+    content: "Текст первой текстовой статьи",
+    created_at: "01.06.2026"
+  },
+  {
+    title: "Втораая статья",
+    content: "Текст второй статьи",
+    created_at: "01.06.2026"
+  }
+]
